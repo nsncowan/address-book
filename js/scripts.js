874984f45dfbe29fb2ed12746be804e88a553872
Noah Cowan <nsncowan@gmail.com>
2022-11-27 10:47:42 -0800

    add address book, and contacts object constructors.
    Co-Authored-By: Ariel Doyle <ariel.rose.garrett@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Co-Authored-By: Ariel Doyle <ariel.rose.garrett@gmail.com>
+
+// AddressBook
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.firstName] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+
+
+
