a0cb72b60876d308f75592251383373f533cfebb
Noah Cowan <nsncowan@gmail.com>
2022-11-28 19:13:11 -0800

    add functionality to display address and email.
    Co-authored by: Chloe O'Neil <chloesporri@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -33,16 +33,61 @@ AddressBook.prototype.deletecontact = function(id) {
 
 
 // Business Logic for Contacts
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, workEmail, personalEmail, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.workEmail = workEmail;
+  this.personalEmail = personalEmail;
+  this.address = address;
+  
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+// User Interface Logic
+let addressBook = new AddressBook;
+
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText = null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    const li2 = document.createElement("li");
+    const li3 = document.createElement("li");
+    const li4 = document.createElement("li");
+    const li5 = document.createElement("li");
+
+    li.append(contact.fullName());
+    li3.append(contact.address);
+    li4.append(contact.workEmail);
+    li5.append(contact.personalEmail);
+    li.setAttribute("id", contact.id);
+    ul.append(li, li3, li4, li5);
+  });
+  contactsDiv.append(ul);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  const inputtedWorkEmail = document.querySelector("input#new-work-email").value;
+  const inputtedPersonalEmail = document.querySelector("input#new-personal-email").value;
+  const inputtedAddress = document.querySelector("input#new-address").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedWorkEmail, inputtedPersonalEmail, inputtedAddress);
+  addressBook.addContact(newContact);
+  listContacts(addressBook);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
 
 
 
